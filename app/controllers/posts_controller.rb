3322c5b2f53939495b6c71ba084a0a0445296d5e
shepitko <ya.shepitko@gmail.com>
2016-08-02 21:38:13 +0300

    refactoring PostsConstroller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,44 +1,32 @@
 class PostsController < ApplicationController
   before_action :load_post, only: [:show, :edit, :update, :destroy]
   before_action :authorize_admin, except: [:show, :index]
-  respond_to :html
-  responders :flash
+  
   def index
     @posts = Post.all
   end
-  
-  def show
-  end
-  
+
   def new
     @post = Post.new
   end
 
-  def edit
-  end
-
   def create
-    @post = Post.new(post_params)
-    if @post.save
-      respond_with @post
-    else
-      render :new
-    end
+    @post = Post.create(post_params)
+    respond_with @post
   end
 
   def update
-    if @post.update(post_params)
-      redirect_to @post
-    else
-      render :edit
-    end
+    @post.update(post_params)
+    respond_with @post
   end
 
   def destroy
     @post.destroy
     redirect_to posts_path
   end
+
   private
+
   def post_params
     params.require(:post).permit(:post_img, :title, :content)
   end
@@ -48,10 +36,6 @@ class PostsController < ApplicationController
   end
 
   def authorize_admin
-    if user_signed_in?
-      redirect_to posts_path, alert: 'Admins only!' unless current_user.admin?
-    else
-      redirect_to posts_path, alert: 'Admins only!'
-    end
+    redirect_to posts_path, alert: 'Admins only!' unless user_signed_in? && current_user.admin?
   end
 end
